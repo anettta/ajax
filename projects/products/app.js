f7884c4c54f9c16a37e706b20701a2775571360b
Anya <annagapyuk@gmail.com>
2022-03-29 12:38:22 -0400

    new project

---
+++ projects/products/app.js
@@ -0,0 +1,23 @@
+const url = "https://course-api.com/javascript-store-products";
+
+const productsDOM = document.querySelector(".products-center");
+
+const fetchProducts = async () => {
+  productsDOM.innerHTML = `<div class="loading"></div>`;
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    productsDOM.innerHTML = `<p class="error"> there was an error</p>`;
+  }
+};
+
+const displayProducts = (list) => {};
+
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+
+start();
