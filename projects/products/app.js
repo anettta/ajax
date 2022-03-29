6850eeb76121603b51ef8ebb16e1bb009e64b390
Anya <annagapyuk@gmail.com>
2022-03-29 13:20:04 -0400

    single product page in progress

--- projects/products/app.js
+++ projects/products/app.js
@@ -13,11 +13,36 @@ const fetchProducts = async () => {
   }
 };
 
-const displayProducts = (list) => {};
+const displayProducts = (list) => {
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      const formatPrice = price / 100;
+      // id, name, price, img
+      // !!! to fetch single product: link + &${id}
+      return ` <a class="single-product" href="product.html?id=${id}"> 
+            <img
+              src="${img}"
+              class="single-product-img img"
+              alt="${title}"
+            />
+            <footer>
+              <h5 class="name">${title}</h5>
+              <span class="price">${formatPrice}</span>
+            </footer>
+          </a>`;
+    })
+    .join("");
+  productsDOM.innerHTML = `<div class="products-container>
+    ${productList}
+    </div>`;
+};
 
 const start = async () => {
   const data = await fetchProducts();
   displayProducts(data);
 };
 
-start();
+start(); // both fetchProducts() and displayProducts in one start() function
